5bf144afcd303f97df1b55a0c40f4a6b55adc92c
plusmancn <plusmancn@gmail.com>
2019-10-25 12:35:11 +0800

    feat: sort bubble

--- src/cn/plusman/sort/Bubble.java
+++ src/cn/plusman/sort/Bubble.java
@@ -4,6 +4,25 @@ import java.util.Arrays;
 
 public class Bubble {
     static int[] sort(int[] a) {
+        if(a.length == 1) return a;
+
+        for(int i = 0; i < a.length; i++) {
+            boolean inOrder = true;
+            for(int j = 0; j < a.length - i - 1; j++) {
+                if(a[j] > a[j+1]) {
+                    int tmp = a[j+1];
+                    a[j+1] = a[j];
+                    a[j] = tmp;
+
+                    inOrder = false;
+                }
+            }
+
+            if(inOrder) {
+                break;
+            }
+        }
+
         return a;
     }
 
