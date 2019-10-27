b0f9099a26db245b677e118400bc5edfb26051e1
plusmancn <plusmancn@gmail.com>
2019-10-27 20:00:15 +0800

    feat: add mergeSort

--- src/cn/plusman/sort/Insert.java
+++ src/cn/plusman/sort/Insert.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Insert {
-    static int[] sort(int[] a) {
-        if(a.length <= 1) return a;
+    static void sort(int[] a) {
+        if(a.length <= 1) return;
 
         for(int i = 1; i < a.length; i++) {
             int temp = a[i];
@@ -21,14 +21,12 @@ public class Insert {
 
             a[j+1] = temp;
         }
-
-        return a;
     }
 
     public static void main(String[] args) {
         int[] arr = {4, 5, 6, 1, 3, 2};
         System.out.println(Arrays.toString(arr));
-        arr = sort(arr);
+        sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
