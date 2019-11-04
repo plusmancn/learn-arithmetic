9849b4f29ae98c12ecd36664c57924d3b30229ba
plusmancn <plusmancn@gmail.com>
2019-11-04 21:53:45 +0800

    feat: update binarySearchAhead

--- src/cn/plusman/search/Search1.java
+++ src/cn/plusman/search/Search1.java
@@ -7,7 +7,7 @@ public class Search1 {
      * @param a
      * @return int
      */
-    public static int mySearch(int[] a, int key) {
+    public static int searchAhead(int[] a, int key) {
         int low = 0;
         int high = a.length -1;
         int mid = -1;
@@ -16,12 +16,11 @@ public class Search1 {
             mid = (low + high) >>> 1;
 
             if(a[mid] == key) {
-                // 向前搜索，直到搜到非 key 值
-                while (a[mid -1] == key) {
-                    mid = mid - 1;
+                if(mid != 0 && a[mid -1] == key) {
+                    high = mid - 1;
+                } else {
+                    return mid;
                 }
-
-                return mid;
             } else if(a[mid] > key) {
                 high = mid - 1;
             } else if(a[mid] < key) {
@@ -32,8 +31,9 @@ public class Search1 {
         return -1;
     }
 
+
     public static void main(String[] args) {
         int[] a = {1, 3, 4, 5, 6, 8, 8, 8, 11, 18};
-        System.out.println(mySearch(a, 8));
+        System.out.println(searchAhead(a, 8));
     }
 }
