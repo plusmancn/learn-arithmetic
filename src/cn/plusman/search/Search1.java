ed4b346746608e442eb62117bcf5494b75423754
plusmancn <plusmancn@gmail.com>
2019-11-05 00:12:44 +0800

    feat: searchBackwards

--- src/cn/plusman/search/Search1.java
+++ src/cn/plusman/search/Search1.java
@@ -1,11 +1,12 @@
 package cn.plusman.search;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Search1 {
 
     /**
      * 查找第一个值等于给定值的元素
-     * @param a
-     * @return int
      */
     public static int searchAhead(int[] a, int key) {
         int low = 0;
@@ -31,9 +32,49 @@ public class Search1 {
         return -1;
     }
 
+    /**
+     * 查找最后一个值等于给定值的元素
+     */
+    public static int searchBackwards(int[] a, int key) {
+        int low = 0;
+        int high = a.length -1;
+        int mid = -1;
+
+        while (low <= high) {
+            mid = (low + high) >>> 1;
+
+            if(a[mid] == key) {
+                if(mid != a.length - 1 && a[mid + 1] == key) {
+                    low = mid + 1;
+                } else {
+                    return mid;
+                }
+            } else if(a[mid] > key) {
+                high = mid - 1;
+            } else if(a[mid] < key) {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * 查找第一个大于等于给定值的元素
+     */
+
+    /**
+     * 查找最后一个小于等于给定值的元素
+     */
+
+
 
     public static void main(String[] args) {
-        int[] a = {1, 3, 4, 5, 6, 8, 8, 8, 11, 18};
-        System.out.println(searchAhead(a, 8));
+        int[] a = {1, 2, 3, 4, 5, 8, 8, 8, 8, 10};
+        System.out.println("searchAhead: " + searchAhead(a, 8));
+        System.out.println("searchBackwards: " + searchBackwards(a, 8));
+        System.out.println("binarySearch：" + Arrays.binarySearch(a, 8));
+
+        
     }
 }
