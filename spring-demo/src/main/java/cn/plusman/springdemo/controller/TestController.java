5678252b0056f48f98ff13e214e7bfd1fe1e839d
plusman <liujianan@souche.com>
2021-03-03 00:17:01 +0800

    feat: 复现 httpclient timeout waiting for connection from pool 错误

---
+++ spring-demo/src/main/java/cn/plusman/springdemo/controller/TestController.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2021. <plusmancn@gmail.com> All Rights Reversed.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+
+package cn.plusman.springdemo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author plusman
+ * @since 2021/3/2 11:48 PM
+ */
+@Slf4j
+@RestController
+public class TestController {
+    /**
+     *
+     * @param time 单位毫秒
+     * @return
+     */
+    @GetMapping("/test/block")
+    public String block(
+        @RequestParam(required = false) int time
+    ) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            log.error("block error", e);
+        }
+    
+        return String.format("blocked %sms", time);
+    }
+    
+}
