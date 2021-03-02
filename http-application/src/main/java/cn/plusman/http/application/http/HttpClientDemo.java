b926a17318313fbb66024ddaf4c753bf98adaeeb
plusman <liujianan@souche.com>
2021-03-03 00:17:17 +0800

    feat: 复现 httpclient timeout waiting for connection from pool 错误

--- http-application/src/main/java/cn/plusman/http/application/http/HttpClientDemo.java
+++ http-application/src/main/java/cn/plusman/http/application/http/HttpClientDemo.java
@@ -1,5 +1,8 @@
 package cn.plusman.http.application.http;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -9,6 +12,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * cn.plusman.http.application.http
@@ -16,17 +23,60 @@ import java.io.IOException;
  * @author plusman
  * @since 1/2/21
  */
+@Slf4j
 public class HttpClientDemo {
     private static Logger LOGGER = LoggerFactory.getLogger(HttpClientDemo.class);
+    
+    final static CloseableHttpClient httpClient;
+    static {
+        RequestConfig requestConfig = RequestConfig.custom()
+            // 从连接池获取连接的等待时间
+            .setConnectionRequestTimeout(5000)
+            .setConnectTimeout(5000)
+            .setSocketTimeout(5000)
+            .build();
+        
+        httpClient = HttpClientBuilder
+            .create()
+            .setDefaultRequestConfig(requestConfig)
+            .setMaxConnTotal(1)
+            .build();
+    }
+    
+    private static int count  = 0;
+    private static List<HttpEntity> memoryLeak = new LinkedList<>();
+    public static void postDemo(int time) throws IOException {
+        log.info("enter {}", count++);
+        
+        HttpGet httpGet = new HttpGet("http://127.0.0.1:8080/test/block?time=" + time);
+        
+        // 关闭资源释放
+        CloseableHttpResponse response = httpClient.execute(httpGet);
+        memoryLeak.add(response.getEntity());
+        String body = EntityUtils.toString(response.getEntity(), "UTF-8");
+        log.info("count: {}, {}", count, body);
+    }
 
-    final static CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-
-
-    public static void main(String[] args) throws IOException {
-        HttpGet httpGet = new HttpGet("https://www.baidu.com");
-        try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
-            String body = EntityUtils.toString(response.getEntity(), "UTF-8");
-            LOGGER.info(body);
+    public static void main(String[] args) {
+        try {
+            postDemo(6000);
+        } catch (IOException e) {
+            log.error("catch connection timeout", e);
+        }
+    
+    
+        ExecutorService executors =  Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 10; i++) {
+            executors.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        postDemo(2000);
+                    } catch (IOException e) {
+                        log.error("catch error {}" , e);
+                    }
+                }
+            });
         }
     }
 }
