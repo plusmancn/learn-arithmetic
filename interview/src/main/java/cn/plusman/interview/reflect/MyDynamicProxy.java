e389f4bbce4d94268b3954b8c09462977a888adc
plusman <liujianan@souche.com>
2021-03-29 12:48:45 +0800

    feat: proxy priview

--- interview/src/main/java/cn/plusman/interview/reflect/MyDynamicProxy.java
+++ interview/src/main/java/cn/plusman/interview/reflect/MyDynamicProxy.java
@@ -5,20 +5,24 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 /**
+ * 参考文档：https://www.baeldung.com/java-dynamic-proxies
  * cn.plusman.interview.reflect
- *
  * @author plusman
  * @since 12/15/20
  */
 public class MyDynamicProxy {
     public static void main(String[] args) {
         HelloImpl helloImpl = new HelloImpl();
-        MyInvocationHandler handler = new MyInvocationHandler(helloImpl);
-
-        Hello proxyHello = (Hello) Proxy.newProxyInstance(Hello.class.getClassLoader(), new Class[] {Hello.class}, handler);
-
+        MyInvocationHandler handler = new MyInvocationHandler();
+        // 第二个参数必须呀是 interface 类型，不然无法校验通过
+        // 此处 HelloImpl.class 是 class 类型
+        Hello proxyHello = (HelloImpl) Proxy.newProxyInstance(Hello.class.getClassLoader(), new Class[]{Hello.class}, handler);
         // proxyHello.sayHello();
-        proxyHello.sayHello("plusman");
+        // proxyHello.sayHello("plusman");
+        
+        HelloIndependent hi = new HelloIndependent();
+        
+        hi.pong();
     }
 }
 
@@ -32,13 +36,21 @@ interface Hello {
     void sayHello(String name);
 }
 
+/**
+ * 无任何接口抽象
+ */
+class HelloIndependent {
+    public void pong() {
+        System.out.println("Hello pong");
+    }
+}
 
 class HelloImpl implements Hello {
     @Override
     public void sayHello() {
         System.out.println("Hello world");
     }
-    
+
     @Override
     public void sayHello(String name) {
         System.out.println("Hello " + name);
@@ -51,11 +63,22 @@ class MyInvocationHandler implements InvocationHandler {
     public MyInvocationHandler(Object target) {
         this.targetCC = target;
     }
-
+    
+    public MyInvocationHandler() {
+    }
+    
+    /**
+     * proxy != handler
+     * proxy 是通过字节码生成的接口实例化对象
+     * @param proxy
+     * @param method
+     * @param args
+     * @return
+     * @throws Throwable
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        System.out.println("Invoking sayHello");
-        Object result = method.invoke(targetCC, args);
-        return result;
+        System.out.println(method.getName() + " was invoked");
+        return null;
     }
 }
